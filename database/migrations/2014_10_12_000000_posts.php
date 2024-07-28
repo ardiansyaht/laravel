11e144f3ade72696d03bb84b010e0525a5c5a97a
ardiansyaht <mccalister2306@gmail.com>
2024-07-28 20:51:27 +0700

    fix posts

--- database/migrations/2014_10_12_000000_posts.php
+++ database/migrations/2014_10_12_000000_posts.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->string('title');
-            $table->string('content');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
